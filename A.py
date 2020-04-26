2ef486f5e6ba6e11e80a960e921ee9b615887d7a
子墨 <461009747@qq.com>
2020-04-26 15:05:53 +0800

    更新示例，增加对CLOUD认证方式的支持

--- A.py
+++ A.py
@@ -2,15 +2,22 @@ import requests
 
 
 def getCookies():
+    # params = {
+    #     'login_url': 'http://authserver.swun.edu.cn/authserver/login',
+    #     'needcaptcha_url': 'http://authserver.swun.edu.cn/authserver/needCaptcha.html',
+    #     'captcha_url': 'http://authserver.swun.edu.cn/authserver/captcha.html',
+    #     'username': 'test',
+    #     'password': 'test'
+    # }
+
     params = {
-        'login_url': 'http://authserver.swun.edu.cn/authserver/login',
-        'needcaptcha_url': 'http://authserver.swun.edu.cn/authserver/needCaptcha.html',
-        'captcha_url': 'http://authserver.swun.edu.cn/authserver/captcha.html',
+        'login_url': 'https://jjjs.campusphere.net/iap/login',
+        'needcaptcha_url': 'https://jjjs.campusphere.net/iap/checkNeedCaptcha',
+        'captcha_url': 'https://jjjs.campusphere.net/iap/generateCaptcha',
         'username': 'test',
         'password': 'test'
     }
-
-    res = requests.post('http://www.zimo.wiki:8080/yibinu-score-crawler/api/login', params)
+    res = requests.post('http://localhost:8080/yibinu-score-crawler/api/login', params)
     print(res.text)
 
 
