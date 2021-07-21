e7372ad0cc3697afe280704e2f15eee056af83f1
HongjieCN <57511284+HongjieCN@users.noreply.github.com>
2021-07-21 13:45:17 +0800

    Update rlMessage.py

--- 今日校园/actions/rlMessage.py
+++ 今日校园/actions/rlMessage.py
@@ -4,20 +4,30 @@ import requests
 # 若离消息通知类
 class RlMessage:
     # 初始化类
-    def __init__(self, mail, apiUrl):
+    def __init__(self, mail, wx):
         self.mail = mail
-        self.apiUrl = apiUrl
-
+        self.wx = wx
     # 发送邮件消息
     def sendMail(self, status, msg):
         # 若离邮件api， 将会存储消息到数据库，并保存1周以供查看，请勿乱用，谢谢合作
         if self.mail == '':
             return '邮箱为空，已取消发送邮件！'
+        api = 'http://mail.ruoli.cc/api/sendMail'
         params = {
             'to': self.mail,
             'title': f'[{status}]今日校园通知',
             'content': msg
         }
-        res = requests.post(url=self.apiUrl, params=params).json()
+        res = requests.post(url=api, params=params).json()
         return res['msg']
-    # 其他通知方式待添加
+    # Server酱Turbo通知
+    def sendWX(self, status, msg):
+        if self.wx=='':
+            return '微信为空，已取消发送微信！'
+        api = 'https://sctapi.ftqq.com/{wx}.send'.format(wx=self.wx)
+        data = {
+            'title': msg,
+            "desp": f'[{status}]今日校园通知'
+        }
+        res=requests.post(api,data = data)
+        return res.json()['data']['error']
